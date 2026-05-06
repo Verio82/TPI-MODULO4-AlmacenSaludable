9c13a491dcf91272a67f27801d34c21e7581f9c4
veromuzzio <veromuzzio@gmail.com>
2026-05-06 16:10:43 -0300

    ajustando el puerto para render

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.use((err, req, res, next) => {
 });
 
 //Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
 
 module.exports.logger = logger; // Exportamos el logger para usarlo en otras partes de la app
